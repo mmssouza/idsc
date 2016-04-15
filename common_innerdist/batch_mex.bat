627208f5ce8eb614309e03a244a8f064c9de8364
Marcelo@Sony notebook <marcelo.mssouza@gmail.com>
2016-04-15 10:20:02 -0300

    added batch_mex.bat

---
+++ common_innerdist/batch_mex.bat
@@ -0,0 +1,9 @@
+
+set OCTAVE_PATH=c:\Octave\Octave-4.0.1
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 bellman_ford_ex_C.cpp
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 build_graph_contour_C.cpp
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 dist2_C.cpp
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 DPMatching_C.cpp
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 uniform_interp_C.cpp
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 dist_bw_sc_C.c
+%OCTAVE_PATH%\bin\mkoctfile.exe --mex -I%OCTAVE_PATH%\include\octave-4.0.1\octave -L%OCTAVE_PATH%\lib\octave\4.0.1 hist_cost.c
